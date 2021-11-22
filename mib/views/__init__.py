fc05ccc310c03f6f7644b1596f0ca2d3b8ed559c
paololanza <paolo.lanza.97@gmail.com>
2021-11-22 19:38:08 +0100

    user profile added

--- mib/views/__init__.py
+++ mib/views/__init__.py
@@ -1,7 +1,8 @@
 from .auth import auth
 from .home import home
 from .users import users
+from .filters import filters
 
 """List of the views to be visible through the project
 """
-blueprints = [home, auth, users]
+blueprints = [home, auth, users, filters]
