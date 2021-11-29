43fa4f2480c60522307cd172b8359a66bbcae627
paololanza <paolo.lanza.97@gmail.com>
2021-11-29 14:57:18 +0100

    notifications added

--- mib/rao/notification_manager.py
+++ mib/rao/notification_manager.py
@@ -1,16 +1,29 @@
 import requests
 from mib import app
+from typing import Tuple
 
 class NotificationManager:
     NOTIFICATIONS_ENDPOINT = app.config['NOTIFICATIONS_MS_URL']
     REQUESTS_TIMEOUT_SECONDS = app.config['REQUESTS_TIMEOUT_SECONDS']
 
     @classmethod
-    def get_notifications(cls) -> int:
+    def get_notifications(cls) -> Tuple[int, str]:
         endpoint = f"{cls.NOTIFICATIONS_ENDPOINT}/notifications"
         try:
             response = requests.get(endpoint, timeout=cls.REQUESTS_TIMEOUT_SECONDS)
+            data = response.json()
+            notifications = data.get("data")
+            return response.status_code, notifications
+        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+            return 500, "Unexpected reponse from user microservice"
+
+    @classmethod
+    def add_notifications(cls, data) -> int:
+        endpoint = f'{cls.NOTIFICATIONS_ENDPOINT}/notifications/add'
+        try:
+            response = requests.post(endpoint, data=data, timeout=cls.REQUESTS_TIMEOUT_SECONDS)
             return response.status_code
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-            return 500
+            return 500, "Unexpected reponse from user microservice"
+        
 
