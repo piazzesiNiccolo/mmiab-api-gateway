b96aedacd656676c0be5599759834c11aac619fa
nicco <npiazzesi@gmail.com>
2021-12-01 23:03:55 +0100

    added tests for repo template code

--- mib/rao/notification_manager.py
+++ mib/rao/notification_manager.py
@@ -2,14 +2,19 @@ import requests
 from mib import app
 
 class NotificationManager:
-    NOTIFICATIONS_ENDPOINT = app.config['NOTIFICATIONS_MS_URL']
-    REQUESTS_TIMEOUT_SECONDS = app.config['REQUESTS_TIMEOUT_SECONDS']
+    @classmethod
+    def notifications_endpoint(cls):
+        return app.config['NOTIFICATIONS_MS_URL']
+    
+    @classmethod
+    def requests_timeout_seconds(cls):
+        return app.config['REQUESTS_TIMEOUT_SECONDS']
 
     @classmethod
     def get_notifications(cls) -> int:
-        endpoint = f"{cls.NOTIFICATIONS_ENDPOINT}/notifications"
+        endpoint = f"{cls.notifications_endpoint()}/notifications"
         try:
-            response = requests.get(endpoint, timeout=cls.REQUESTS_TIMEOUT_SECONDS)
+            response = requests.get(endpoint, timeout=cls.requests_timeout_seconds())
             return response.status_code
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
             return 500
---
+++ tests/test_login_manager.py
@@ -0,0 +1,23 @@
+from flask_login import login_manager
+from flask_login.mixins import AnonymousUserMixin
+import pytest
+import mock
+from flask import current_app
+from flask_login import login_user
+from mib.auth.user import User
+from mib.auth.login_manager import init_login_manager
+class TestLoginManager:
+    
+    @pytest.mark.parametrize("user",[ 
+        None,
+        User(id=1, email="email@email.com", is_active=False, authenticated=False, is_anonymous=False,extra='')
+    ])
+    def test_init_login_manager(self,user):
+        with mock.patch("mib.rao.user_manager.UserManager.get_user_by_id") as m:
+            m.return_value = user, None
+            mgr = init_login_manager(current_app)
+            assert mgr.login_view == 'auth.login'
+            assert mgr.refresh_view == 'auth.re_login'
+            mgr.user_loader(1)
+            
+            
